374fcacfbd048e8d126b0da3d2418dfa32348c08
MarcinChamera <MChamera237@gmail.com>
2019-09-16 21:11:12 +0200

    a lot of features to be implemented

---
+++ AMAZEING/app/src/main/java/com/example/a_maze_ing/WelcomeScreenActivity.java
@@ -0,0 +1,39 @@
+package com.example.a_maze_ing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+public class WelcomeScreenActivity extends AppCompatActivity {
+    public int buttonID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void selectSize(View view) {
+        Intent intent;
+        buttonID = view.getId();
+        if(buttonID == R.id.button6) {
+            intent = new Intent(this, CustomSizeActivity.class);
+        } else {
+            intent = new Intent(this, MainActivity.class);
+            Button button = findViewById(buttonID);
+            int [] mazeSize = new int [2];
+            /* rows */
+            mazeSize[0] = Integer.parseInt(button.getText().toString().substring(0, 2));
+            /* columns */
+            mazeSize[1] = Integer.parseInt(button.getText().toString().substring(0, 2));
+            intent.putExtra("mazeSize", mazeSize);
+        }
+        startActivity(intent);
+    }
+}
